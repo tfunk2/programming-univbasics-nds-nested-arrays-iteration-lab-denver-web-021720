25d5b7154c2d9d84feb819f6493e268491d78a91
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-05 23:08:55 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -54,6 +54,8 @@ def total_even_pairs(src)
   while row_index < src.count do
     if (src[row_index][0] % 2) && (src[row_index][1] % 2) == 0
       total += (src[row_index][0] + src[row_index][1])
+    end
+    row_index +=1
   end
   total
 end
